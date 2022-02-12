e5663070a77fec9522a5747e843c4c7475e562d6
Виталий Колесник <boston07080@gmail.com>
2022-02-12 15:09:45 +0300

    Version 2/0

---
+++ ZooGuard.Web/Models/VenderViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZooGuard.Web.Models
+{
+    public class VenderViewModel
+    {
+        [HiddenInput] //Генерирование скрытого поля ввода
+        public int? Id { get; set; }
+
+        [Required, MaxLength(50), Display(Name = "Company")]
+        public string Name { get; set; } 
+
+        [Required, MaxLength(200), Display(Name = "First Name")]
+        public string FirstNameRepresentative { get; set; }
+
+        [Required, MaxLength(200), Display(Name = "Last Name")]
+        public string LastNameRepresentative { get; set; }
+
+        [Required, MaxLength(15), Display(Name = "Phone")]
+        public string PhoneRepresentative { get; set; }
+
+        [Required, MaxLength(15), Display(Name = "Email")]
+        public string EmailRepresentative { get; set; }
+
+        [Required, MaxLength(50), Display(Name = "Adress")]
+        public string MailingAddress { get; set; }
+
+        [Required, MaxLength(200), Display(Name = "About vender")]
+        public string Сomment { get; set; }
+    }
+}
---
+++ ZooGuard.Web/Services/PositionViewModelService.cs
@@ -0,0 +1,57 @@
+﻿using ZooGuard.Core.Entities;
+using ZooGuard.Core.Entities.InfoAboutPos;
+using ZooGuard.Core.Interfaces;
+using ZooGuard.Web.Interfaces;
+using ZooGuard.Web.Models;
+
+namespace ZooGuard.Web.Services
+{
+    public class PositionViewModelService : IPositionViewModelService
+    {
+        private readonly IPositionService positionService;
+
+        public PositionViewModelService(IPositionService positionService)
+        {
+            this.positionService = positionService;
+        }
+
+        public int Add(PositionViewModel positionViewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Edit(PositionViewModel position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PositionViewModel GetById(int id)
+        {
+            var position = positionService.Get(id);
+            return position != null ? ConvertToViewModel(position) : null;
+        }
+
+        public PositionViewModel GetEmpty()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private PositionViewModel ConvertToViewModel(Position position) //формирует модель при ихспользовании метода GetById
+        {
+            return new PositionViewModel
+            {
+                Id = position.Id,
+                Name = position.Name,
+                Date = position.Date,
+                RegistrationDocument = position.RegistrationDocument,
+                AccountingNumber = position.AccountingNumber,
+                RealityFlag = position.RealityFlag,
+                Information = position.Information,
+                Vender = position.Vender.Name,
+                FormOfOccurence = position.FormOfOccurence.Name,
+                User = string.Join(position.User.Name, position.User.LastName),
+                StatusLabel = position.StatusLabel.Name,
+            };
+        }
+    }
+}
